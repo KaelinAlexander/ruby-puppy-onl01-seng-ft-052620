c0d2c64c1402b94538e3fde26f5f63204dc973c5
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-05 15:30:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,12 +17,12 @@ class Dog
     end
   end
 
-def save
-  @@all << self
-end
-
-def self.clear_all
-  @all.clear
-end
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @all.clear
+  end
 
 end 
